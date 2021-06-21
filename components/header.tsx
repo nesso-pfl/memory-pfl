3f9523125b9c190c5db3b502989f5af259cb5949
nesso <32949286+nesso-pfl@users.noreply.github.com>
2021-06-21 22:08:54 +0900

    Organize

---
+++ components/header.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import Link from "next/link";
+
+export const Header = () => {
+  return (
+    <header>
+      <Link href="/">memory-pfl</Link>
+    </header>
+  );
+};
