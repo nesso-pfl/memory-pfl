af48a8950e25bf2479eecbaa50b55d2df273205a
nesso <32949286+nesso-pfl@users.noreply.github.com>
2021-06-22 02:34:26 +0900

    Update description page

--- components/index.ts
+++ components/index.ts
@@ -1,2 +1,3 @@
 export { Header } from "./header";
 export { Layout } from "./layout";
+export { Tags } from "./tags";
---
+++ components/tags.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import Link from "next/link";
+
+type Props = {
+  tags: string[];
+};
+
+export const Tags: React.VFC<Props> = ({ tags }) => {
+  return (
+    <ul>
+      {tags.map((tag) => (
+        <li key={tag}>
+          <Link href={`/description/${tag}`}>{tag}</Link>
+        </li>
+      ))}
+    </ul>
+  );
+};
